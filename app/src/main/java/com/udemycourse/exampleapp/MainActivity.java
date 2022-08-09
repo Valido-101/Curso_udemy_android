71a0e6d926e40e7b89168a2b8efe3ba43fca96aa
Valido-101 <jesuszafra22@gmail.com>
2022-08-09 14:19:02 +0200

    Obteniendo valores de un editText en java

--- app/src/main/java/com/udemycourse/exampleapp/MainActivity.java
+++ app/src/main/java/com/udemycourse/exampleapp/MainActivity.java
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
     public void clickFunction(View view){
 
@@ -14,9 +21,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void showName(View view){
+
+        EditText nameEditText = (EditText)findViewById(R.id.NameEditText);
+
+        String name = nameEditText.getText().toString();
+
+        Log.i("Info","Button Pressed!");
+
+        Log.i("Values","Your name: " + name);
+
     }
 }
